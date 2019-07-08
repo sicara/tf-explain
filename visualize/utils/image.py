35a332940b01eb6dabd7fdd79d77cf8897937cbd
Raphael Meudec <raphaelm@sicara.com>
2019-07-08 09:25:00 +0200

    Image utils

---
+++ visualize/utils/image.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def apply_grey_patch(image, top_left_x, top_left_y, patch_size):
+    """
+    Replace a part of the image with a grey patch.
+
+    Args:
+        image (numpy.ndarray): Input image
+        top_left_x (int): Top Left X position of the applied box
+        top_left_y (int): Top Left Y position of the applied box
+        patch_size (int): Size of patch to apply
+
+    Returns:
+
+    """
+    patched_image = np.array(image, copy=True)
+    patched_image[top_left_y:top_left_y + patch_size, top_left_x:top_left_x + patch_size, :] = 127.5
+
+    return patched_image
