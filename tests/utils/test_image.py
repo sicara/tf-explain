b775685380199daab794fca5a2f70aa168ec51e6
Raphael Meudec <raphaelm@sicara.com>
2019-07-08 09:30:28 +0200

    Add test for grey patch applicaiton

---
+++ tests/utils/test_image.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from visualize.utils.image import apply_grey_patch
+
+
+def test_should_apply_grey_patch_on_image():
+    input_image = np.zeros((10, 20, 3))
+
+    output = apply_grey_patch(input_image, 0, 0, 10)
+
+    expected_output = np.concatenate([
+        127.5 * np.ones((10, 10, 3)),
+        np.zeros((10, 10, 3))
+    ], axis=1)
+
+    np.testing.assert_almost_equal(output, expected_output)
+
+
