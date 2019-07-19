ea69f1de25a4da2b21d943b00deb5b3f80c3f069
Raphael Meudec <raphaelm@sicara.com>
2019-07-19 08:53:48 +0200

    Normalize map

--- examples/occlusion_sensitivity.py
+++ examples/occlusion_sensitivity.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import cv2
+import numpy as np
 import tensorflow as tf
 
 from mentat.callbacks.occlusion_sensitivity import OcclusionSensitivityCallback
@@ -19,10 +20,11 @@ if __name__ == '__main__':
     tabby_cat_class_index = 281
     sensitivity_map = OcclusionSensitivityCallback.get_sensitivity_map(model, img, tabby_cat_class_index, 10)
 
+    sensitivity_map = (sensitivity_map - np.min(sensitivity_map)) / (np.max(sensitivity_map) - np.min(sensitivity_map))
     sensitivity_jet_map = cv2.applyColorMap(
-        cv2.cvtColor((sensitivity_map * 255).astype('uint8'), cv2.COLOR_GRAY2RGB),
+        cv2.cvtColor((sensitivity_map * 255).astype('uint8'), cv2.COLOR_GRAY2BGR),
         cv2.COLORMAP_JET,
     )
 
-    heatmap_image = cv2.addWeighted(cv2.cvtColor(img.astype('uint8'), cv2.COLOR_RGB2BGR), 1, sensitivity_jet_map, 0.5, 0)
-    cv2.imwrite(str(Path('../tests/test_logs') / 'occlusion.png'), heatmap_image)
+    heatmap_image = cv2.addWeighted(cv2.cvtColor(img.astype('uint8'), cv2.COLOR_RGB2BGR), 0.5, sensitivity_jet_map, 1, 0)
+    cv2.imwrite(str(Path('../') / 'occlusion.png'), heatmap_image)
