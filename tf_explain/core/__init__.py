2090549196853d78ccd1e2fc9139ff1186cc4520
Raphael Meudec <raphaelm@sicara.com>
2019-07-27 09:52:04 +0200

    Add to init

--- tf_explain/core/__init__.py
+++ tf_explain/core/__init__.py
@@ -1,6 +1,7 @@
 from .activations import ExtractActivations
 from .grad_cam import GradCAM
 from .occlusion_sensitivity import OcclusionSensitivity
+from .smoothgrad import SmoothGrad
 
 
-__all__ = ["ExtractActivations", "GradCAM", "OcclusionSensitivity"]
+__all__ = ["ExtractActivations", "GradCAM", "OcclusionSensitivity", "SmoothGrad"]
