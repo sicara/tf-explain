6e4d69f56b802ad6f05fa259ffb83e24a4abe7cb
Raphael Meudec <raphaelm@sicara.com>
2019-07-27 09:38:49 +0200

    Add SmoothGrad core

---
+++ tests/core/test_smoothgrad.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from tf_explain.core.smoothgrad import SmoothGrad
+
+
+def test_should_explain_output(convolutional_model, random_data, mocker):
+    mocker.patch("tf_explain.core.smoothgrad.grid_display", side_effect=lambda x: x)
+    images, labels = random_data
+    explainer = SmoothGrad()
+    grid = explainer.explain((images, labels), convolutional_model, 0)
+
+    assert grid.shape == images.shape
+
+
+def test_generate_noisy_images(mocker):
+    input_shape = (10, 28, 28, 1)
+    num_samples = 3
+    mocker.patch(
+        "tf_explain.core.smoothgrad.np.random.normal",
+        return_value=np.ones((30, 28, 28, 1)),
+    )
+
+    images = np.ones(input_shape)
+    output = SmoothGrad.generate_noisy_images(images, num_samples=num_samples, noise=1)
+
+    np.testing.assert_array_equal(output, 2 * np.ones((30, 28, 28, 1)))
+
+
+def test_get_averaged_gradients(random_data, convolutional_model):
+    images, _ = random_data
+    num_samples = 2
+    gradients = SmoothGrad.get_averaged_gradients(
+        images, convolutional_model, 0, num_samples=num_samples
+    )
+
+    expected_output_shape = (images.shape[0] / num_samples, *images.shape[1:])
+
+    assert gradients.shape == expected_output_shape
---
+++ tf_explain/core/smoothgrad.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import cv2
+import numpy as np
+import tensorflow as tf
+
+from tf_explain.utils.display import grid_display, heatmap_display
+
+
+class SmoothGrad:
+
+    """
+    Perform Grad CAM algorithm for a given input
+
+    Paper: [Grad-CAM: Visual Explanations from Deep Networks
+            via Gradient-based Localization](https://arxiv.org/abs/1610.02391)
+    """
+
+    def explain(self, validation_data, model, class_index, num_samples=5, noise=1.0):
+        images, _ = validation_data
+
+        noisy_images = SmoothGrad.generate_noisy_images(images, num_samples, noise)
+
+        smoothed_gradients = SmoothGrad.get_averaged_gradients(
+            noisy_images, model, class_index, num_samples
+        ).numpy()
+
+        grid = grid_display(smoothed_gradients)
+
+        return grid
+
+    @staticmethod
+    def generate_noisy_images(images, num_samples, noise):
+        repeated_images = np.repeat(images, num_samples, axis=0)
+        noise = np.random.normal(0, noise, repeated_images.shape).astype(np.float32)
+
+        return repeated_images + noise
+
+    @staticmethod
+    @tf.function
+    def get_averaged_gradients(noisy_images, model, class_index, num_samples):
+        num_classes = model.output.shape[1]
+
+        expected_output = tf.one_hot([class_index] * len(noisy_images), num_classes)
+
+        with tf.GradientTape() as tape:
+            inputs = tf.cast(noisy_images, tf.float32)
+            tape.watch(inputs)
+            predictions = model(inputs)
+            loss = tf.keras.losses.categorical_crossentropy(
+                expected_output, predictions
+            )
+
+        grads = tape.gradient(loss, inputs)
+
+        grads_per_image = tf.reshape(grads, (-1, num_samples, *grads.shape[1:]))
+        averaged_grads = tf.reduce_mean(grads_per_image, axis=1)
+
+        return averaged_grads
+
+    def save(self, grid, output_dir, output_name):
+        Path.mkdir(Path(output_dir), parents=True, exist_ok=True)
+
+        cv2.imwrite(
+            str(Path(output_dir) / output_name), cv2.cvtColor(grid, cv2.COLOR_RGB2BGR)
+        )
