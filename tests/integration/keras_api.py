7932ab527a3f2443e42cedc580f7bc707a2f9ba0
Raphael Meudec <raphaelm@sicara.com>
2019-07-30 15:11:41 +0200

    Lint

--- tests/integration/keras_api.py
+++ tests/integration/keras_api.py
@@ -10,71 +10,108 @@ INPUT_SHAPE = (28, 28, 1)
 def functional_api_model(num_classes):
     img_input = tf.keras.Input(INPUT_SHAPE)
 
-    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(img_input)
-    x = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu")(
+        img_input
+    )
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu")(x)
     x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
 
-    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(x)
-    x = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', name='grad_cam_target')(x)
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu")(x)
+    x = tf.keras.layers.Conv2D(
+        filters=64, kernel_size=(3, 3), activation="relu", name="grad_cam_target"
+    )(x)
     x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
 
     x = tf.keras.layers.Flatten()(x)
 
-    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    x = tf.keras.layers.Dense(64, activation="relu")(x)
 
-    x = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+    x = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
 
     model = tf.keras.Model(img_input, x)
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     return model
 
 
 def sequential_api_model(num_classes):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=INPUT_SHAPE),
-        tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
-        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
-
-        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
-        tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', name='grad_cam_target'),
-        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
-
-        tf.keras.layers.Flatten(),
-
-        tf.keras.layers.Dense(64, activation='relu'),
-
-        tf.keras.layers.Dense(num_classes, activation='softmax'),
-    ])
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Conv2D(
+                filters=32,
+                kernel_size=(3, 3),
+                activation="relu",
+                input_shape=INPUT_SHAPE,
+            ),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.Conv2D(
+                filters=64,
+                kernel_size=(3, 3),
+                activation="relu",
+                name="grad_cam_target",
+            ),
+            tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     return model
 
 
 def subclassing_api_model(num_classes):
     class SubclassedModel(tf.keras.models.Model):
-        def __init__(self, name='subclassed'):
+        def __init__(self, name="subclassed"):
             super(SubclassedModel, self).__init__(name=name)
-            self.conv_1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')
-            self.conv_2 = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu')
+            self.conv_1 = tf.keras.layers.Conv2D(
+                filters=32, kernel_size=(3, 3), activation="relu"
+            )
+            self.conv_2 = tf.keras.layers.Conv2D(
+                filters=64, kernel_size=(3, 3), activation="relu"
+            )
             self.maxpool_1 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))
 
-            self.conv_3 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')
-            self.conv_4 = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', name='grad_cam_target')
+            self.conv_3 = tf.keras.layers.Conv2D(
+                filters=32, kernel_size=(3, 3), activation="relu"
+            )
+            self.conv_4 = tf.keras.layers.Conv2D(
+                filters=64,
+                kernel_size=(3, 3),
+                activation="relu",
+                name="grad_cam_target",
+            )
             self.maxpool_2 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))
 
             self.flatten = tf.keras.layers.Flatten()
 
-            self.dense_1 = tf.keras.layers.Dense(64, activation='relu')
+            self.dense_1 = tf.keras.layers.Dense(64, activation="relu")
 
-            self.dense_2 = tf.keras.layers.Dense(num_classes, activation='softmax')
+            self.dense_2 = tf.keras.layers.Dense(num_classes, activation="softmax")
 
         def build(self, input_shape):
             super(SubclassedModel, self).build(input_shape)
 
         def call(self, inputs, **kwargs):
             x = inputs
-            for layer in [self.conv_1, self.conv_2, self.maxpool_1, self.conv_3, self.conv_4, self.maxpool_2, self.flatten, self.dense_1, self.dense_2]:
+            for layer in [
+                self.conv_1,
+                self.conv_2,
+                self.maxpool_1,
+                self.conv_3,
+                self.conv_4,
+                self.maxpool_2,
+                self.flatten,
+                self.dense_1,
+                self.dense_2,
+            ]:
                 x = layer(x)
 
             return x
@@ -85,16 +122,19 @@ def subclassing_api_model(num_classes):
 
     model = SubclassedModel()
 
-    model(np.random.random([4, *INPUT_SHAPE]).astype('float32'))  # Sample call to build the model
+    model(
+        np.random.random([4, *INPUT_SHAPE]).astype("float32")
+    )  # Sample call to build the model
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     return model
 
 
-
 # TODO: Activate subclassing API model (issue #55)
-@pytest.mark.parametrize('model_builder', [functional_api_model, sequential_api_model])
+@pytest.mark.parametrize("model_builder", [functional_api_model, sequential_api_model])
 def test_all_keras_api(model_builder, mnist_dataset, validation_dataset, num_classes):
     train_images, train_labels, test_images, test_labels = mnist_dataset
 
@@ -105,9 +145,15 @@ def test_all_keras_api(model_builder, mnist_dataset, validation_dataset, num_cla
 
     # Instantiate callbacks
     callbacks = [
-        tf_explain.callbacks.GradCAMCallback(validation_data, 'grad_cam_target', class_index=target_class),
-        tf_explain.callbacks.ActivationsVisualizationCallback(validation_data, 'grad_cam_target'),
-        tf_explain.callbacks.SmoothGradCallback(validation_data, class_index=target_class, num_samples=15, noise=1.),
+        tf_explain.callbacks.GradCAMCallback(
+            validation_data, "grad_cam_target", class_index=target_class
+        ),
+        tf_explain.callbacks.ActivationsVisualizationCallback(
+            validation_data, "grad_cam_target"
+        ),
+        tf_explain.callbacks.SmoothGradCallback(
+            validation_data, class_index=target_class, num_samples=15, noise=1.0
+        ),
     ]
 
     # Start training
