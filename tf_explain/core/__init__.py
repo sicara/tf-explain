45e717b413701b6ba515babd84a3fe64d155db1e
Raphael Meudec <raphaelm@sicara.com>
2019-09-18 16:23:16 +0200

    Add IntegratedGradients in init

--- tf_explain/core/__init__.py
+++ tf_explain/core/__init__.py
@@ -1,7 +1,14 @@
 from .activations import ExtractActivations
 from .grad_cam import GradCAM
+from .integrated_gradients import IntegratedGradients
 from .occlusion_sensitivity import OcclusionSensitivity
 from .smoothgrad import SmoothGrad
 
 
-__all__ = ["ExtractActivations", "GradCAM", "OcclusionSensitivity", "SmoothGrad"]
+__all__ = [
+    "ExtractActivations",
+    "GradCAM",
+    "IntegratedGradients",
+    "OcclusionSensitivity",
+    "SmoothGrad",
+]
