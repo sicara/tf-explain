a16b726cebc12e9a2701129f9adc0d0fdc53b7c9
Raphael Meudec <raphaelm@sicara.com>
2019-09-20 16:28:43 +0200

    Add vanilla gradients example

---
+++ examples/core/vanilla_gradients.py
@@ -0,0 +1,22 @@
+import numpy as np
+import tensorflow as tf
+
+from tf_explain.core.gradients import VanillaGradients
+
+
+IMAGE_PATH = './cat.jpg'
+
+if __name__ == '__main__':
+    model = tf.keras.applications.vgg16.VGG16(weights='imagenet', include_top=True)
+
+    img = tf.keras.preprocessing.image.load_img(IMAGE_PATH, target_size=(224, 224))
+    img = tf.keras.preprocessing.image.img_to_array(img)
+
+    model.summary()
+    data = (np.array([img]), None)
+
+    tabby_cat_class_index = 281
+    explainer = VanillaGradients()
+    # Compute VanillaGradients on VGG16
+    grid = explainer.explain(data, model, tabby_cat_class_index)
+    explainer.save(grid, '.', 'vanilla_gradients.png')
