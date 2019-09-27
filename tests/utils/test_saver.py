7ff1d8641e1aaaa5c3e8c599efef2f9f113bb778
Raphael Meudec <raphaelm@sicara.com>
2019-09-27 14:23:04 +0200

    Int type images

--- tests/utils/test_saver.py
+++ tests/utils/test_saver.py
@@ -6,7 +6,7 @@ from tf_explain.utils.saver import save_grayscale, save_rgb
 
 
 def test_should_save_grayscale_image(output_dir):
-    input_image = np.ones((28, 28, 1))
+    input_image = np.ones((28, 28, 1), dtype="uint8")
 
     save_grayscale(input_image, output_dir, "grayscale.png")
 
