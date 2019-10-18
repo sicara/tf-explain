562f666d79b9f3103acc7a365cfa92944cdf5a6b
yannickw <yannickw@sicara.com>
2019-10-18 19:34:08 +0200

    Add GradientsInputsCallback

--- tests/callbacks/test_vanilla_gradients.py
+++ tests/callbacks/test_vanilla_gradients.py
@@ -6,20 +6,19 @@ from tf_explain.callbacks.vanilla_gradients import VanillaGradientsCallback
 def test_should_call_vanilla_gradients_callback(
     random_data, convolutional_model, output_dir, mocker
 ):
+    images, labels = random_data
+
+    vanilla_gradient_callback = VanillaGradientsCallback(
+        validation_data=random_data, class_index=0, output_dir=output_dir
+    )
+
     mock_explainer = mocker.MagicMock(
         explain=mocker.MagicMock(return_value=np.zeros((28, 28)))
     )
-    mocker.patch(
-        "tf_explain.callbacks.vanilla_gradients.VanillaGradients", return_value=mock_explainer
-    )
 
-    images, labels = random_data
+    vanilla_gradient_callback.explainer = mock_explainer
 
-    callbacks = [
-        VanillaGradientsCallback(
-            validation_data=random_data, class_index=0, output_dir=output_dir
-        )
-    ]
+    callbacks = [vanilla_gradient_callback]
 
     convolutional_model.fit(images, labels, batch_size=2, epochs=1, callbacks=callbacks)
 
--- tf_explain/callbacks/__init__.py
+++ tf_explain/callbacks/__init__.py
@@ -5,6 +5,7 @@ This module regroups all the methods as tf.keras.Callback.
 """
 from .activations_visualization import ActivationsVisualizationCallback
 from .grad_cam import GradCAMCallback
+from .gradients_inputs import GradientsInputsCallback
 from .vanilla_gradients import VanillaGradientsCallback
 from .integrated_gradients import IntegratedGradientsCallback
 from .occlusion_sensitivity import OcclusionSensitivityCallback
@@ -18,4 +19,5 @@ __all__ = [
     "OcclusionSensitivityCallback",
     "SmoothGradCallback",
     "VanillaGradientsCallback",
+    "GradientsInputsCallback",
 ]
