Fix Vanilla Gradients callback test.

tests/callbacks/test_vanilla_gradients.py
@@ -7,13 +7,15 @@ def test_should_call_vanilla_gradients_callback(
     random_data, convolutional_model, output_dir, mocker
 ):
     images, labels = random_data
+    score_model = convolutional_model
 
     vanilla_gradient_callback = VanillaGradientsCallback(
         validation_data=random_data, class_index=0, output_dir=output_dir
     )
 
     mock_explainer = mocker.MagicMock(
-        explain=mocker.MagicMock(return_value=np.zeros((28, 28)))
+        _get_score_model=mocker.MagicMock(return_value=score_model),
+        _explain_score_model=mocker.MagicMock(return_value=np.zeros((28, 28)))
     )
 
     vanilla_gradient_callback.explainer = mock_explainer
@@ -21,6 +23,7 @@ def test_should_call_vanilla_gradients_callback(
     callbacks = [vanilla_gradient_callback]
 
     convolutional_model.fit(images, labels, batch_size=2, epochs=1, callbacks=callbacks)
-
-    mock_explainer.explain.assert_called_once_with(random_data, convolutional_model, 0)
+    
+    mock_explainer._get_score_model.assert_called_once()
+    mock_explainer._explain_score_model.assert_called_once_with(random_data, score_model, 0)
     assert len(list(output_dir.iterdir())) == 1