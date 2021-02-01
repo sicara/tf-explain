cd7b9d84a8d843258f299a71ecccaa777eaa6e48
Alex Kubiesa <alex.kubiesa@outlook.com>
2021-02-01 23:07:59 +0000

    Add another test to make sure that the score model only gets computed once.

--- tests/callbacks/test_vanilla_gradients.py
+++ tests/callbacks/test_vanilla_gradients.py
@@ -27,3 +27,35 @@ def test_should_call_vanilla_gradients_callback(
     mock_explainer._get_score_model.assert_called_once()
     mock_explainer._explain_score_model.assert_called_once_with(random_data, score_model, 0)
     assert len(list(output_dir.iterdir())) == 1
+
+
+def test_should_only_compute_score_model_once(
+    random_data, convolutional_model, output_dir, mocker
+):
+    """
+    Tests that the Vanilla Gradients explainer only computes the score model once
+    during training. This improves performance as it ensures the gradients are
+    always calculated with the same score model, which prevents tf.function retracing.
+    """
+
+    images, labels = random_data
+    score_model = convolutional_model
+
+    vanilla_gradient_callback = VanillaGradientsCallback(
+        validation_data=random_data, class_index=0, output_dir=output_dir
+    )
+
+    mock_explainer = mocker.MagicMock(
+        _get_score_model=mocker.MagicMock(return_value=score_model),
+        _explain_score_model=mocker.MagicMock(return_value=np.zeros((28, 28)))
+    )
+
+    vanilla_gradient_callback.explainer = mock_explainer
+
+    callbacks = [vanilla_gradient_callback]
+
+    # Two epochs
+    convolutional_model.fit(images, labels, batch_size=2, epochs=2, callbacks=callbacks)
+    
+    # Score model only computed once
+    mock_explainer._get_score_model.assert_called_once()
