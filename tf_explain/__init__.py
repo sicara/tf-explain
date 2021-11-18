59bdd3ca7a2bfcdf36a4cd96b54902adc4773998
Raphael Meudec <raphael.meudec@inria.fr>
2021-11-18 21:25:06 +0100

    Release 0.3.0 => 0.3.1 (#178)
    * Add citation file in bumpversion config
    
    * Bump version: 0.3.0 → 0.3.1

--- tf_explain/__init__.py
+++ tf_explain/__init__.py
@@ -5,7 +5,7 @@ The library implements interpretability methods as Tensorflow 2.0
 callbacks to ease neural network's understanding.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 try:
     import cv2
